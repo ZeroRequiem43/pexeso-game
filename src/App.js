354bead72ad60fefbc7115ca9674a81cf79ee96d
Zero <knight_altair1@mail.ru>
2021-12-07 16:55:30 +0300

    Added some stuff

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import './App.css'
+import './assets/scss/index.scss'
+import Header from './components/Header'
+import { MainPage } from './pages/MainPage'
+import { GamePage } from './pages/GamePage'
+// import M from 'materialize-css'
 
-function App() {
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header />
+        <Routes >
+          <Route path="/" element={<MainPage />} />
+          <Route path="/game" element={<GamePage />} />
+        </Routes >
+      </Router>
     </div>
   );
 }
-
-export default App;
